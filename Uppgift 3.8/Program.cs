1c690d0de52d80883a4d35020db25dce2b875936
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-06 13:02:22 +0200

    Add project files.

---
+++ Uppgift 3.8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift3._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett ord: ");
+            string ord1 = Console.ReadLine();
+            Console.WriteLine("Skriv ett annat ord: ");
+            string ord2 = Console.ReadLine();
+            Console.WriteLine("Skriv ett tredje ord: ");
+            string ord3 = Console.ReadLine();
+
+            if (ord1.CompareTo(ord2) < 0 && ord1.CompareTo(ord3) < 0)
+            {
+                Console.WriteLine("\"" + ord1 + "\" kommer före \"" + ord2 + "\" och \"" + ord3 + "\" i bokstavsordning");
+            }
+
+            else if (ord2.CompareTo(ord1) < 0 && ord2.CompareTo(ord3) < 0)
+            {
+                Console.WriteLine("\"" + ord2 + "\" kommer före \"" + ord1 + "\" och \"" + ord3 + "\" i bokstavsordning");
+            }
+
+            else
+            {
+                Console.WriteLine("\"" + ord3 + "\" kommer före \"" + ord1 + "\" och \"" + ord2 + "\" i bokstavsordning");
+            }
+        }
+    }
+}
